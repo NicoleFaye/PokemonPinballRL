1ba0216bde246767a3d319e1d2525dec75bbac4f
Nicole Demera <Nicolefayedemera@gmail.com>
2025-04-23 21:12:20 -0700

    puffer removal

--- environment/__init__.py
+++ environment/__init__.py
@@ -1,4 +1,3 @@
 """Pokemon Pinball RL environment initialization."""
 from environment.rewards import RewardShaping
 from environment.pokemon_pinball_env import PokemonPinballEnv, Actions
-from environment.wrappers import PufferWrapper
