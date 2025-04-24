bfcaca8f0bd830dd8f54a02b660adb4f16573a86
Nicole Demera <Nicolefayedemera@gmail.com>
2025-04-23 19:37:35 -0700

    init

---
+++ train.py
@@ -0,0 +1,96 @@
+import sys
+from os.path import exists
+from pathlib import Path
+from stable_baselines3 import PPO
+from stable_baselines3.common import env_checker
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.callbacks import CheckpointCallback, CallbackList
+from environment.pokemon_pinball_env import PokemonPinballEnv
+
+def make_env(rank, env_conf, seed=0):
+    """
+    Utility function for multiprocessed env.
+    :param env_id: (str) the environment ID
+    :param num_env: (int) the number of environments you wish to have in subprocesses
+    :param seed: (int) the initial seed for RNG
+    :param rank: (int) index of the subprocess
+    """
+    def _init():
+        env = PokemonPinballEnv("./roms/pokemon_pinball.gbc",env_conf)
+        env.reset(seed=(seed + rank))
+        return env
+    set_random_seed(seed)
+    return _init
+
+if __name__ == "__main__":
+
+    use_wandb_logging = False
+    ep_length = 2048 * 80
+    sess_id = "runs"
+    sess_path = Path(sess_id)
+
+    env_config = {
+        'headless': True,
+        'debug': False,
+        'reward_shaping': 'comprehensive',
+        'info_level': 2,
+        'frame_stack': 4,
+        'frame_skip': 3,
+        'visual_mode': 'screen',
+        'frame_stack_extra_observation': False,
+        'reduce_screen_resolution': True
+    }
+    
+    print(env_config)
+    
+    num_cpu = 1 # Also sets the number of episodes per training iteration
+    env = SubprocVecEnv([make_env(i, env_config) for i in range(num_cpu)])
+    
+    checkpoint_callback = CheckpointCallback(save_freq=ep_length//2, save_path=sess_path,
+                                     name_prefix="poke")
+    
+    callbacks = []#[checkpoint_callback, TensorboardCallback(sess_path)]
+
+    if use_wandb_logging:
+        import wandb
+        from wandb.integration.sb3 import WandbCallback
+        wandb.tensorboard.patch(root_logdir=str(sess_path))
+        run = wandb.init(
+            project="pokemon-train",
+            id=sess_id,
+            name="v2-a",
+            config=env_config,
+            sync_tensorboard=True,  
+            monitor_gym=True,  
+            save_code=True,
+        )
+        callbacks.append(WandbCallback())
+
+    #env_checker.check_env(env)
+
+    # put a checkpoint here you want to start from    
+    if sys.stdin.isatty():
+        file_name = ""
+    else:
+        file_name = sys.stdin.read().strip() #"runs/poke_26214400_steps"
+
+    train_steps_batch = ep_length // 64
+    
+    if exists(file_name + ".zip"):
+        print("\nloading checkpoint")
+        model = PPO.load(file_name, env=env)
+        model.n_steps = train_steps_batch
+        model.n_envs = num_cpu
+        model.rollout_buffer.buffer_size = train_steps_batch
+        model.rollout_buffer.n_envs = num_cpu
+        model.rollout_buffer.reset()
+    else:
+        model = PPO("MultiInputPolicy", env, verbose=1, n_steps=train_steps_batch, batch_size=512, n_epochs=1, gamma=0.997, ent_coef=0.01, tensorboard_log=sess_path)
+    
+    print(model.policy)
+
+    model.learn(total_timesteps=(ep_length)*num_cpu*10000, callback=CallbackList(callbacks), tb_log_name="poke_ppo")
+
+    if use_wandb_logging:
+        run.finish()
