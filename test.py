2d11a3d0fe8fb68703a17215ad9b2cd915109756
Nicole Demera <Nicolefayedemera@gmail.com>
2025-05-20 13:14:00 -0700

    remove keyboard usage with new signal ending

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 from pyboy import PyBoy
-import keyboard
 import signal # Aggressively exit on ctrl+c
 from os import _exit
 signal.signal(signal.SIGINT, lambda sig, frame: _exit(0))
@@ -20,12 +19,3 @@ while True:
     print(f"{count}")
     print(f"Balls left: {boy.game_wrapper.balls_left}")
     print(f"Lost ball during saver: {boy.game_wrapper.lost_ball_during_saver}")
-
-
-    # Wait for any key press
-    keyboard.read_event(suppress=True)
-    
-    # Check if the pressed key is 'q' to exit
-    if keyboard.is_pressed('q'):
-        print("Exiting loop!")
-        break
